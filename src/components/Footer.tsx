69550e46aaa734c7cf7d469db1c927519ba5f215
assilbekov <askhat.assilbekov@gmail.com>
2024-04-06 05:34:20 -0300

    add Footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,36 @@
 import { WordMark } from "@/components/WordMark";
+import Link from "next/link";
+
+const navigationItems = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+]
 
 export const Footer = () => {
   return (
-    <div>
+    <footer>
       <WordMark />
-      Footer
-    </div>
+      <nav>
+        <ul>
+          {navigationItems.map((item) => (
+            <li key={item.href}>
+              <Link href={item.href}>
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </footer>
   )
 }
