42ea92b7084645e9c1f925ebf4439930fdb0f68e
assilbekov <askhat.assilbekov@gmail.com>
2024-04-07 05:01:25 -0300

    add hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,17 @@
-import HeroImage from "../../public/hero.jpg";
+import Image from "next/image";
 import { Bounded } from "./Bounded";
 import StarGrid from "./StarGrid";
+import HeroImage from "../../public/images/hero-image.png";
 
 export const Hero = () => {
   return (
     <Bounded>
       <StarGrid />
+      <Image
+        src={HeroImage}
+        style={{ width: "100vw", height: "auto" }}
+        alt="hero"
+      />
     </Bounded>
   );
 };
